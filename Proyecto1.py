f859ec66326cfbc7c74f7d64e4ffb9d0f0f51bd4
Erick Hernández <erickher_2000@hotmail.com>
2024-07-04 11:11:10 -0600

    Mi primer commit

---
+++ Proyecto1.py
@@ -0,0 +1,5 @@
+print('Hola, buen día')
+nombre = input('Ingresa tu nombre:')
+edad = int(input('Ingresa tu edad:'))
+año = 2024 - edad
+print(f'Tu nombre es {nombre}, tienes {edad} años, naciste en el año {año}')
