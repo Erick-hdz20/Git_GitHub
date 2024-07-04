4bc38ce5d9a04e1b322ec1651b894c45c6748954
Erick Hernández <erickher_2000@hotmail.com>
2024-07-04 11:17:56 -0600

    Se agregaron unos comentarios

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,5 +1,10 @@
+#Mostrar saludo
 print('Hola, buen día')
+#Pedir nombre
 nombre = input('Ingresa tu nombre:')
+#Pedir edad
 edad = int(input('Ingresa tu edad:'))
+#Obtener año
 año = 2024 - edad
+#Mostrar mensaje final
 print(f'Tu nombre es {nombre}, tienes {edad} años, naciste en el año {año}')
